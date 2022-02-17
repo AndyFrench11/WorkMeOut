4c337d06a54be3dd6d581a354688dd852058d6d9
andrew-french <andrew.french@xero.com>
2022-02-17 16:28:35 +1300

    Initial Commit

---
+++ WorkMeOut/WorkMeOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkMeOutApp.swift
+//  WorkMeOut
+//
+//  Created by Andrew French on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkMeOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
